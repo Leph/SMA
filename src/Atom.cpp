d70a9289a3de71c1a05dae930a5824c9e7fc575c
Quentin Rouxel <Quentin.Rouxel@enseirb-matmeca.fr>
2012-11-05 09:09:15 +0100

    Base Atom class

---
+++ src/Atom.cpp
@@ -0,0 +1,65 @@
+#include <assert.h>
+
+#include "Atom.hpp"
+#include "Global.hpp"
+
+using Ogre::Real;
+using Ogre::Vector3;
+using Ogre::Entity;
+
+Atom::Atom(Real radius, Vector3 position) :
+    _radius(radius),
+    _position(position),
+    _force(Vector3(0, 0, 0)),
+    _node(0)
+{
+    initNode();
+}
+
+Real Atom::getRadius() const
+{
+    return _radius;
+}
+
+const Vector3& Atom::getPosition() const
+{
+    return _position;
+}
+
+void Atom::move(Real dt)
+{
+    assert(dt > 0);
+    _position += _force*dt;
+    _node->setPosition(_position);
+}
+
+void Atom::computeMove()
+{
+    _force = Vector3(0, 0, 0);
+    _force += applyGravity();
+    _force += applyBrownian();
+}
+
+void Atom::initNode()
+{
+    //Creer une sphere de rayon _radius pour la représentation 
+    //graphique de l'atome
+    Entity* sphere = Global::getSceneManager()
+        ->createEntity(Ogre::SceneManager::PT_SPHERE);
+    _node = Global::getSceneManager()
+        ->getRootSceneNode()->createChildSceneNode();
+    _node->attachObject(sphere);
+    _node->setScale(Vector3(_radius, _radius, _radius));
+    _node->setPosition(_position);
+}
+
+Vector3 Atom::applyGravity()
+{
+    return Vector3(0, 0, -2.0);
+}
+
+Vector3 Atom::applyBrownian()
+{
+    return Vector3(0, 0, 0);
+}
+
---
+++ src/Atom.hpp
@@ -0,0 +1,89 @@
+#ifndef ATOM_HPP
+#define ATOM_HPP
+
+#include <Ogre.h>
+
+/**
+ * Classe de base pour toutes les particules
+ */
+class Atom
+{
+    public:
+
+        /**
+         * Construit et initialise l'atome
+         */
+        Atom(Ogre::Real radius, Ogre::Vector3 position);
+
+        /**
+         * Renvoi le rayon
+         */
+        Ogre::Real getRadius() const;
+
+        /**
+         * Renvoi la position
+         */
+        const Ogre::Vector3& getPosition() const;
+
+        /**
+         * Déplace l'atome
+         * Intégration des forces directement sur la position
+         * Déplacement du node d'Ogre de la scène
+         * Tests des collisions
+         *
+         * @param dt : pas de temps
+         */
+        void move(Ogre::Real dt);
+
+        /**
+         * Réinitialise et calcul les forces
+         * appliquée à l'atome
+         * Donne le vecteur de déplacement
+         */
+        void computeMove();
+
+    protected:
+
+        /**
+         * Le rayon de l'atome
+         */
+        Ogre::Real _radius;
+
+        /**
+         * Sa position
+         */
+        Ogre::Vector3 _position;
+
+        /**
+         * La somme des forces instantannées qui
+         * lui sont appliqué (ex : gravité, mouvement brownien)
+         */
+        Ogre::Vector3 _force;
+
+        /**
+         * Le noeud Ogre auquel est rataché la représentation
+         * graphique de l'atome
+         */
+        Ogre::SceneNode* _node;
+
+        /**
+         * Créer et initialise le Node Ogre pour
+         * la représentation graphique
+         */
+        void initNode();
+
+        /**
+         * Renvoi la force de gravitation
+         * appliquée sur l'atome
+         */
+        Ogre::Vector3 applyGravity();
+
+        /**
+         * Renvoi le mouvement brownien
+         * appliqué sur l'atome
+         */
+        Ogre::Vector3 applyBrownian();
+};
+
+#endif
+
